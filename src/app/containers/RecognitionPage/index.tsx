f467ea30078523f6912279e5ff4d831b192f416d
hassan11196 <hassan11196@hotmail.com>
2020-12-03 08:26:54 +0500

    Add FaceCrop

--- src/app/containers/RecognitionPage/index.tsx
+++ src/app/containers/RecognitionPage/index.tsx
@@ -6,7 +6,48 @@ import { PageWrapper } from 'app/components/PageWrapper';
 import * as faceapi from 'face-api.js';
 import Webcam from 'react-webcam';
 
-let NETLIFY_URL = 'recognize-and-recommend.netlify.app'
+let NETLIFY_URL = 'recognize-and-recommend.netlify.app';
+
+function image64toCanvasRef(
+    canvasRef: HTMLCanvasElement | null,
+    image64,
+    pixelCrop,
+) {
+    return new Promise(resolve => {
+
+
+        const canvas = canvasRef; // document.createElement('canvas');
+        // @ts-ignore
+        canvas.width = pixelCrop.width;
+        // @ts-ignore
+        canvas.height = pixelCrop.height;
+        // @ts-ignore
+        const ctx = canvas.getContext('2d');
+        const image = new Image();
+        image.src = image64;
+        image.onload = function (e) {
+
+            // @ts-ignore
+            ctx.drawImage(
+                image,
+                pixelCrop.x,
+                pixelCrop.y,
+                pixelCrop.width,
+                pixelCrop.height,
+                0,
+                0,
+                pixelCrop.width,
+                pixelCrop.height,
+            );
+            // @ts-ignore
+            resolve(canvas?.toDataURL());
+
+
+        };
+        // @ts-ignore
+        // return canvas.toDataURL();
+    })
+}
 
 export function RecognitionPage() {
     const videoConstraints = {
@@ -23,13 +64,20 @@ export function RecognitionPage() {
         if (process.env.NODE_ENV == 'development') {
             await faceapi.nets.ssdMobilenetv1
                 .loadFromUri('/models')
-                .then(error => console.log('ssdMobilenetv1 Model Loaded', error));
+                .then(error => {
+                    console.log('ssdMobilenetv1 Model Loaded', error);
+                    alert("Model Loaded. Face Detection Will Start Now.")
+                }
 
+                );
         } else {
             await faceapi.nets.ssdMobilenetv1
                 .loadFromUri('/models')
-                .then(error => console.log('ssdMobilenetv1 Model Loaded', error));
-
+                .then(error => {
+                    console.log('ssdMobilenetv1 Model Loaded', error);
+                    alert("Model Loaded. Face Detection Will Start Now.")
+                }
+                );
         }
 
         // const input = document.getElementsByTagName('video')[0];
@@ -48,21 +96,40 @@ export function RecognitionPage() {
             // @ts-ignore
             const detection = await faceapi.detectSingleFace(input);
             if (detection != undefined) {
-
-
                 console.log(detection);
 
                 const displaySize = videoConstraints;
                 const canvas = document.getElementById('overlay');
 
                 // @ts-ignore
-                const dims = faceapi.matchDimensions(canvas, input, true)
+                const dims = faceapi.matchDimensions(canvas, input, true);
                 // @ts-ignore
-                faceapi.draw.drawDetections(canvas, faceapi.resizeResults(detection, displaySize))
+                faceapi.draw.drawDetections(
+                    // @ts-ignore
+                    canvas,
+                    faceapi.resizeResults(detection, displaySize),
+                );
 
+                const croppedCanvas = document.getElementById('croppedOverlay');
+                // @ts-ignore
+                const base64Image = webcamRef?.current?.getScreenshot();
+                // console.log(base64Image)
+                const myCrop = {
+                    x: detection.box.x,
+                    y: detection.box.y,
+                    width: detection.box.width,
+                    height: detection.box.height,
+                };
+                let croppedBase64 = await image64toCanvasRef(
+                    // @ts-ignore
+                    croppedCanvas,
+                    base64Image,
+                    myCrop,
+                );
+                // @ts-ignore
+                console.log(croppedBase64);
             }
-
-        }, 1000);
+        }, 2000);
     };
 
     useEffect(() => {
@@ -77,8 +144,41 @@ export function RecognitionPage() {
             </Helmet>
             {/* <NavBar /> */}
             <PageWrapper>
-                <Webcam style={{ position: "absolute", top: 0, left: 0, backgroundSize: "cover", width: "100%", minHeight: "100%" }} ref={webcamRef} videoConstraints={videoConstraints} />
-                <canvas style={{ position: "absolute", top: 0, left: 0, backgroundSize: "cover", width: "100%", minHeight: "100%" }} id="overlay"></canvas>
+                <Webcam
+                    style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        backgroundSize: 'cover',
+                        width: '100%',
+                        minHeight: '100%',
+                    }}
+                    ref={webcamRef}
+                    videoConstraints={videoConstraints}
+                />
+                <canvas
+                    style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        backgroundSize: 'cover',
+                        width: '100%',
+                        minHeight: '100%',
+                    }}
+                    id="overlay"
+                ></canvas>
+                <canvas
+                    style={{
+                        display: "none",
+                        position: 'relative',
+                        top: 0,
+                        left: 0,
+                        backgroundSize: 'cover',
+                        width: '100%',
+                        minHeight: '100%',
+                    }}
+                    id="croppedOverlay"
+                ></canvas>
             </PageWrapper>
         </>
     );
